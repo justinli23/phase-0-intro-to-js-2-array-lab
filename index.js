9ff3a64037f5b838f67d5fce52d0ee490ba4e064
Justin Li <justinlit23@gmail.com>
2022-04-06 10:41:45 -0700

    commit tree f43cc4cc979e7ea2f2d625b598a5c3f94adbcd7f

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat () {
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat () {
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat () {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat () {
+    cats.shift();
+}
+
+function appendCat () {
+    const copyOfCats = [...cats,"Broom"];
+    return copyOfCats;
+}
+
+function prependCat () {
+    const copyOfCats = ["Arnold", ...cats];
+    return copyOfCats;
+}
+
+function removeLastCat () {
+    const copyOfCats = cats.slice(0,-1);
+    return copyOfCats;
+}
+
+function removeFirstCat () {
+    const copyOfCats = cats.slice(1);
+    return copyOfCats;
+}
